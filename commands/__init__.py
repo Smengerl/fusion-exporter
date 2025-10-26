c3c98bcdce439007bc626721905627e2ef6f27b4
Simon Gerlach <simon.gerlach@gmail.com>
2025-10-26 14:24:56 +0100

    Initial commit

---
+++ commands/__init__.py
@@ -0,0 +1,5 @@
+"""Auto GitHub Exporter - Automatic exporter of fusion files in the common format I used in my GitHub projects"""
+
+__version__ = '0.1.0'
+__author__ = 'Simon Gerlach <my@email.com>'
+__all__ = []
